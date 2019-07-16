1f8cd1bb9835d444e148010c4aafe7fe2a94dc24
Evan Jeffries <ejeffries14@gmail.com>
2019-07-16 21:47:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
   []
 end
-hero = ["Tony Stark","Bruce Banner","Nick Fury","Steve Rogers"]
+
 def create_an_array
-  
+ hero = ["Tony Stark","Bruce Banner","Nick Fury","Steve Rogers"] 
 end
 
 def add_element_to_end_of_array(array, element)
