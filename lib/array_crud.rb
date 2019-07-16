041cce86d416eca34ce7d04bb987fab868057164
Evan Jeffries <ejeffries14@gmail.com>
2019-07-16 21:45:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,7 +1,7 @@
 def create_an_empty_array
   []
 end
-["Tony Stark","Bruce Banner","Nick Fury","Steve Rogers"]
+hero.new["Tony Stark","Bruce Banner","Nick Fury","Steve Rogers"]
 def create_an_array
   
 end
